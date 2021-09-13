068a0592bafe91f02a8e69e574925eb833a2778d
Tristan Bullard <tbullard@vrbo.com>
2021-09-13 11:34:35 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+Amplify.configure(awsExports);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
